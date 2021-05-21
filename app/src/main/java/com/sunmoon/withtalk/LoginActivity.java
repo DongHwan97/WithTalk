8d70704bfd8d614c49915f20683acc1e1aba5770
DongHwan97 <dhpak97@naver.com>
2021-05-21 22:45:10 +0900

    TTS 화면변경 및 채팅화면추가

--- app/src/main/java/com/sunmoon/withtalk/LoginActivity.java
+++ app/src/main/java/com/sunmoon/withtalk/LoginActivity.java
@@ -2,21 +2,25 @@ package com.sunmoon.withtalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText editIDText, editPWText;
-    private TextView moveSignUpText, moveFindIDText, moveResetPWText;
-    private CheckBox autoLoginBox;
-    private Button loginButton;
-    private String id, pw;
 
+    private EditText editIDText, editPWText;
+    private CheckBox autoLogin;
+    private Button loginButton;
+    String id, pw;
 
 
     @Override
@@ -24,64 +28,38 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-/*        SharedPreferences auto = getSharedPreferences("auto", MODE_PRIVATE);
-
-        id = auto.getString("ID",null);
-        pw = auto.getString("PW",null);
-
-        if(id != null &&pw!=null){
-            if(id.equals("12345678") && pw.equals("12345678")){
-                Util.startToast(this,"자동로그인 성공");
-                moveActivity(MainActivity.class);
-            }
-        }*/
-
-
-
         findViewById(R.id.moveSignUp).setOnClickListener(onClickListener);
         findViewById(R.id.moveFindID).setOnClickListener(onClickListener);
         findViewById(R.id.moveResetPW).setOnClickListener(onClickListener);
         findViewById(R.id.loginButton).setOnClickListener(onClickListener);
+
         loginButton = (Button) findViewById(R.id.loginButton);
-        moveSignUpText = (TextView)findViewById(R.id.moveSignUp);
-        moveFindIDText = (TextView)findViewById(R.id.moveFindID);
-        moveResetPWText = (TextView)findViewById(R.id.moveResetPW);
+
         editIDText = (EditText) findViewById(R.id.editID);
-        editPWText = (EditText)findViewById(R.id.editPW);
-        autoLoginBox = (CheckBox)findViewById(R.id.autoLogin);
-
+        editPWText = (EditText) findViewById(R.id.editPW);
+        autoLogin = (CheckBox) findViewById(R.id.autoLogin);
 
     }
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.loginButton:
-
-                    login();
-                    break;
-                case R.id.moveFindID: moveActivity(FindIDActivity.class);
-                    break;
-                case R.id.moveSignUp: moveActivity(SignUpActivity.class);
-                    break;
-                case R.id.moveResetPW: moveActivity(ResetPWActivity.class);
-                    break;
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.loginButton:
+                        moveActivity(MainActivity.class);
+                        break;
+                    case R.id.moveFindID:
+                        moveActivity(FindIDActivity.class);
+                        break;
+                    case R.id.moveSignUp:
+                        moveActivity(SignUpActivity.class);
+                        break;
+                    case R.id.moveResetPW:
+                        moveActivity(ResetPWActivity.class);
+                        break;
+                }
             }
-        }
-    };
+        };
 
-    private void login(){//로그인
-        id = editIDText.getText().toString();
-        pw = editPWText.getText().toString();
-
-        if((id.length()<8)&&(pw.length()<8)){
-            Util.startToast(this,"아이디와 비밀번호는 8자 이상입니다.");
-            //로그인 추가작성필요 TO DO
-        }else{
-            Util.startToast(this,"로그인에 성공하였습니다.");
-            moveActivity(MainActivity.class);
-        }
-    }
 
     @Override
     public void onBackPressed(){
--- app/src/main/java/com/sunmoon/withtalk/TTSFragment.java
+++ app/src/main/java/com/sunmoon/withtalk/TTSFragment.java
@@ -1,15 +1,22 @@
 package com.sunmoon.withtalk;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Locale;
 
 public class TTSFragment extends Fragment {
 
+    public TextToSpeech tts;
 
     public static TTSFragment newInstance() {
         TTSFragment fragment = new TTSFragment();
@@ -20,4 +27,40 @@ public class TTSFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tts, container, false);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        tts = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status== TextToSpeech.SUCCESS){
+                    tts.setLanguage(Locale.KOREAN);
+                }else{
+                    Log.e("TTS", "Initialization Failed");
+                }
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View getView() {
+        changeFragment();
+        return super.getView();
+    }
+
+    public void changeFragment(){
+        tts.speak("미확인메시지 확인 화면", TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    public void onStop(){
+        if(tts!=null){
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onStop();
+    }
+
 }
